36eab743d0ce3aa95a60d522589117b2daad3026
juanvmr <juan97victor@hotmail.com>
2019-09-26 21:53:06 -0300

    Add files via upload

---
+++ exercicios31-33/exercicio32/login.js
@@ -0,0 +1,3 @@
+function error(){
+    alert("Usuário ou senha inválidos");
+}
---
+++ exercicios31-33/exercicio32/server/index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+var app = express();
+
+app.post('/', (req, res)=>{
+    if(req.params.user === ""
+})
